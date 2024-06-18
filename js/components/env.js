1d3325b470b7b24de25f4c67bde4abd27313b553
DiegoFernandoMarinL <160855594+DiegoFernandoMarinL@users.noreply.github.com>
2024-06-18 17:43:45 -0500

    feat: :sparkles: Creacion de busqueda de producto
    Se crea funcion para buscar en la api por medio de la barra de busqueda, ademas se ajusta las vistas checkout.html y detail.html a la carpeta views

--- js/components/env.js
+++ js/components/env.js
@@ -1,4 +1,4 @@
 export const headers = {
-    'x-rapidapi-key': 'f1fa8c8e0fmsh90aa8d98e59634ap190e5ejsnc9d4cc8505b6',
-	'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
+    'x-rapidapi-key': '7f0f45c694mshae909ea61993f5cp1390d7jsn2b108de976d3',
+    'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
 }
