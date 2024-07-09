99aa7185b87d5a5bf7700ce817455b6dadcf6d59
DiegoFernandoMarinL <160855594+DiegoFernandoMarinL@users.noreply.github.com>
2024-07-09 06:46:44 -0500

    chore: :ambulance: Cambio de headers de la api

--- js/components/env.js
+++ js/components/env.js
@@ -1,4 +1,4 @@
 export const headers = {
-    'x-rapidapi-key': '0ae35e6d19msh9c9dbb0e815fca8p10a6c6jsn8ba570d2bcd5',
+    'x-rapidapi-key': '465439138fmshbc2d6a7067e83bcp187524jsn8f89a816621d',
 	'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
 }
